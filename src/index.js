b2405c3cfddbf823c61afb439a9a8c149a089a0a
Denis Selitsky <denis.selitsky@gmail.com>
2023-10-29 18:07:52 +0300

    feat: add services and MarvelService

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from "./components/app/App";
+import MarvelService from './services/MarvelService';
+
+const marvelService = new MarvelService();
+marvelService.getAllCharacters().then(res => res.data.results.forEach(element => console.log(element.name)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
