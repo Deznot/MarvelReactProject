945050c06377702b5fcf250275abf43b3f0aea8e
Denis Selitsky <denis.selitsky@gmail.com>
2023-10-30 22:55:01 +0300

    feat: add CharList via class and service

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -13,9 +13,9 @@ class MarvelService {
     }
 
     getAllCharacters = async () => {
-        let res = await this.getResource(`${this._apiBase}/characters?limit=9&offset=210&${this._apiKey}`);
-        return this._transformCharacterData(res.data.results.map(this._transformCharacterData));
-    };
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacterData);
+    }
 
     getCharacter = async (id) => {
         let res = await this.getResource(`${this._apiBase}/characters/${id}?${this._apiKey}`);
@@ -24,11 +24,12 @@ class MarvelService {
 
     _transformCharacterData = (char) => {
         return {
+            id: char.id,
             name: char.name,
-            description : char.description?`${char.description.slice(0,350)}...`:"data about character is not found",
-            thumbnail : char.thumbnail.path + '.' + char.thumbnail.extension,
-            homepage : char.urls[0].url,
-            wiki: char.urls[1].url,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'Data about character is not found',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
         }
     }
 }
