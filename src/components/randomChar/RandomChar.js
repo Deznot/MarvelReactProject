b8ea1f34aacbc56a15259c7d87297a3fdf068ec1
Denis Selitsky <denis.selitsky@gmail.com>
2023-10-30 16:30:12 +0300

    feat: add RandomChar via componentDidMount and implement CharList via class

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -8,25 +8,24 @@ import ErrorMessage from "../errorMessage/ErrorMessage";
 
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar();
+    state = {
+        char: {},
+        loading: true,
+        error: false,
     }
 
     marvelService = new MarvelService();
 
-    state = {
-       char : {},
-       loading: true,
-       error: false,
+    componentDidMount() {
+        this.updateChar();
     }
 
     onError = () => {
-        this.setState({error: true, loading:false});
+        this.setState({ error: true, loading: false });
     }
 
     onCharLoaded = (char) => {
-        this.setState({char, loading:false});
+        this.setState({ char, loading: false });
     };
 
     updateChar = async () => {
@@ -36,12 +35,17 @@ class RandomChar extends Component {
             .then(this.onCharLoaded)
             .catch(this.onError);
     }
-    
+
+    onClickButtonTry = () => {
+        return this.updateChar();
+    }
+
+
     render() {
-        const {char, loading, error} = this.state;
+        const { char, loading, error } = this.state;
         const errorMessage = error ? <ErrorMessage /> : null;
         const spinner = loading ? <Spinner /> : null;
-        const content = !(loading || errorMessage) ? <View char={char}/> : null;
+        const content = !(loading || errorMessage) ? <View char={char} /> : null;
 
         return (
             <div className="randomChar">
@@ -50,13 +54,13 @@ class RandomChar extends Component {
                 {content}
                 <div className="randomChar__another">
                     <p className="randomChar__title">
-                        Random character for today!<br/>
+                        Random character for today!<br />
                         Do you want to get to know him better?
                     </p>
                     <p className="randomChar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button onClick={this.onClickButtonTry} className="button button__main">
                         <div className="inner">TRY IT</div>
                     </button>
                     <img src={mjolnir} alt="random character decoration" className="randomChar__decoration" />
@@ -66,11 +70,15 @@ class RandomChar extends Component {
     }
 };
 
-const View = ({char}) => {
-    const {thumbnail,name,description,homepage,wiki} = char;
+const View = ({ char }) => {
+    const { thumbnail, name, description, homepage, wiki } = char;
+    let objectFit = 'cover';
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        objectFit = 'contain';
+    }
     return (
         <div className="randomChar__block">
-            <img src={thumbnail} alt="thumbnail" />
+            <img src={thumbnail} alt="thumbnail" style={{ objectFit: objectFit }} />
             <div className="randomChar__info">
                 <p className="randomChar__title">{name}</p>
                 <p className="randomChar__descr">
