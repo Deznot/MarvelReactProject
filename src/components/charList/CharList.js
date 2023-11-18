7439df34d1233a5253baf637287752c7195698c9
Denis Selitsky <denis.selitsky@gmail.com>
2023-11-18 16:47:49 +0300

    feat: change CharList to functional component and fix double call onRequest throught comment strict mod in index.js

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,87 +1,68 @@
-import { Component } from "react";
+import { useState, useEffect, useRef} from "react";
 import PropTypes from "prop-types";
 import "./charList.scss";
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 
-class CharList extends Component {
-    state = {
-        charList: [],
-        loading: true,
-        error: false,
-        newItemLoading: false,
-        offset: 0,
-        charEnded: false
-    }
+const CharList = (props) => {
+    const [charList, setCharList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [newItemLoading, setNewItemLoading] = useState(false);
+    const [offset, setOffset] = useState(0);
+    const [charEnded, setCharEnded] = useState(false);
 
-    marvelService = new MarvelService();
+    let marvelService = new MarvelService();
 
-    componentDidMount() {
-        this.onRequest();
-        // window.addEventListener('scroll', this.onScrollLoading);
-    }
+    useEffect(() => {
+        onRequest();
+        // window.addEventListener('scroll',  onScrollLoading);
+        // return () => {
+        //     window.removeEventListener('scroll', onScrollLoading);
+        // };
+    }, []);
 
-    componentWillUnmount() {    
-        // window.removeEventListener('scroll', this.onScrollLoading);
-    }
-
-    onScrollLoading = () => {
+    const onScrollLoading = () => {
         if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
-            this.onRequest(this.state.offset);
+            onRequest(offset);
         }
     }
 
-    onRequest = (offset) => {
-        this.onCharListLoading();
-        this.marvelService.getAllCharacters(offset)
-            .then(this.onCharListLoaded)
-            .catch(this.onError);
+    const onRequest = (offset) => {
+        onCharListLoading();
+        marvelService.getAllCharacters(offset)
+            .then(onCharListLoaded)
+            .catch(onError);
     }
 
-    onError = () => {
-        this.setState({
-            error: true,
-            loading: false
-        })
+    const onError = () => {
+        setError(true);
+        setLoading(false);
     }
 
-    onCharListLoading = () => {
-        this.setState({
-            newItemLoading: true
-        })
+    const onCharListLoading = () => {
+        setNewItemLoading(true);
     }
 
-    onCharListLoaded = (newCharList) => {
+    const onCharListLoaded = (newCharList) => {
         let ended = false;
         if (newCharList.length < 9) {
             ended = true;
         }
 
-        if (this.state.charList.length !== 0) {
-            if (this.state.charList[0].id === newCharList[0].id) {
-                newCharList = [];
-            }
-        }
-
-        this.setState(({offset, charList}) => ({
-            charList: [...charList, ...newCharList],
-            offset: offset + 9,
-            loading: false,
-            newItemLoading: false,
-            charEnded: ended
-        }));
+        setCharList((charList) => [...charList, ...newCharList]);
+        setOffset((offset) => offset + 9);
+        setLoading(loading => false);
+        setNewItemLoading(false);
+        setCharEnded(ended);
     }
 
-    cardRefs = [];
+    const cardRefs = useRef([]);
 
-    setCardRef = (elem) => {
-        this.cardRefs.push(elem);
-    }
-
-    onCardFocus = (i,id,e) => {
+    const onCardFocus = (i,id,e) => {
         if (e.code === "Enter" || e.code === " ") {
-            this.props.onCharSelected(id)
+            props.onCharSelected(id);
             //через рефы и перебор
             // this.cardRefs.forEach(el => {
             //     el.classList.remove('char__card-active');
@@ -94,16 +75,17 @@ class CharList extends Component {
             switch (e.code) {
                 case 'ArrowLeft' :
                     if (i-1 < 0) {
-                        this.cardRefs[this.cardRefs.length - 1].focus(); 
+                        console.log(cardRefs.current.length);
+                        cardRefs.current[cardRefs.current.length - 1].focus(); 
                     } else {
-                        this.cardRefs[i - 1].focus();
+                        cardRefs.current[i - 1].focus();
                     }
                 break;
                 case "ArrowRight" :
-                    if (i + 1 >= this.cardRefs.length) {
-                        this.cardRefs[0].focus();
+                    if (i + 1 >= cardRefs.current.length) {
+                        cardRefs.current[0].focus();
                     } else {
-                        this.cardRefs[i + 1].focus();
+                        cardRefs.current[i + 1].focus();
                     }
                 break;
                 default : {}
@@ -112,25 +94,25 @@ class CharList extends Component {
         
     }
 
-    renderCard = (charList) => {
+    const renderCard = (charList) => {
         let chars = charList.map((char,i) => {
             const { thumbnail, name, id } = char;
             let objectFit = 'cover';
             if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 objectFit = 'unset';
             }
-            const selected = id === this.props.selectedCharId?"char__card-active": null;
+            const selected = id === props.selectedCharId?"char__card-active": null;
             
             return (
                 <li key={id}
                     onClick={() => {
-                        this.props.onCharSelected(id);
+                        props.onCharSelected(id);
                     }}
                     className= {`char__card ${selected}`}
-                    ref={this.setCardRef}
+                    ref={(el) => cardRefs.current[i] = el}
                     tabIndex={0}
                     onKeyDown={(e) => {
-                        this.onCardFocus(i,id, e);
+                        onCardFocus(i,id, e);
                     }}>
                     <img src={thumbnail} alt={name} className="char__img" style={{ objectFit: objectFit }} />
                     <div className="char__name">{name}</div>
@@ -145,27 +127,26 @@ class CharList extends Component {
         )
     }
 
-    render() {
-        const { charList, loading, error, newItemLoading, offset } = this.state;
-        const dataLoaded = loading ? <Spinner /> : null;
-        const errorMessage = error ? <ErrorMessage /> : null;
-        const cards = !(loading || errorMessage) ? this.renderCard(charList) : null;
+    
+    const dataLoaded = loading ? <Spinner /> : null;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const cards = !(loading || errorMessage) ? renderCard(charList) : null;
 
-        return (
-            <div className="char__list">
-                {dataLoaded}
-                {cards}
-                {errorMessage}
+    return (
+        <div className="char__list">
+            {dataLoaded}
+            {cards}
+            {errorMessage}
 
-                <button 
-                    className="button button__long button__main"
-                    disabled={newItemLoading}
-                    onClick={() => this.onRequest(offset)}>
-                    <div className="inner">LOAD MORE</div>
-                </button>
-            </div>
-        );
-    }
+            <button 
+                className="button button__long button__main"
+                disabled={newItemLoading}
+                onClick={() => onRequest(offset)}>
+                <div className="inner">LOAD MORE</div>
+            </button>
+        </div>
+    );
+    
    
 }
 
